26ed48a5cd340f140ac1a682ac718b3fa1392e7f
GTs-17 <124639113+GTs-17@users.noreply.github.com>
2023-11-24 18:04:49 +0800

    footer js

--- js/header-footer.js
+++ js/header-footer.js
@@ -28,3 +28,69 @@ class header extends HTMLElement {
 }
 
 customElements.define("top-header", header);
+
+
+
+class footer extends HTMLElement {
+    constructor(){
+            super();
+    }
+    connectedCallback() {
+            this.parentNode.innerHTML = `
+                    
+        <section class="row footer_top_section" >
+        <section class="footer_logo ">
+            <a><picture>
+                <img src="image/HomePage_image/logo.jpg" alt="HankookLogo" width="70%">
+            </picture>
+            </a>
+        </section>
+
+        <section class="footer-col">
+            <article>
+                <h4>Companies</h4>
+                <ul>
+                    <li><a href="#">about us</a></li>
+                    <li><a href="#">our services</a></li>
+                    <li><a href="#">privacy policy</a></li>
+                </ul>
+            </article>
+        </section>
+
+        <section class="footer-col input_text">
+            <h4>Санал хүсэлт</h4>
+            <form>
+                <section>
+                    <label for="message">Мессеж</label>
+                    <textarea name="message" id="message" class="texting"></textarea>
+                    <div class="button">
+                        <input type="submit" value="Илгээх">
+                    </div>
+                </section>
+            </form>
+        </section>
+
+
+        <section>
+        <article class="footer-col">
+        <address class="social-links">
+            <h4>Бидэнтэй нэгдэх</h4>
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#"><i class="fa-brands fa-youtube"></i></a>                 
+        </address>
+        </article>
+        </section>
+
+        </section>
+        <div class="footer-line"></div>
+        <section class="footer_bottom_section">
+            <p>
+                © 2023 GTs Chimegee Orgio ХХК. Зохиогчийн эрх хуулиар хамгаалагдсан Developed by <a
+                    href="#">Hankook</a>
+            </p>
+        </section>
+            `;
+    }
+}
+customElements.define("main-footer", footer);
